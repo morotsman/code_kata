7a3b98f5a2ba7f4f31bb8e1ce54418fa193836e9
niklas.leopold <leopold.niklas@gmail.com>
2016-09-23 18:10:33 +0200

    kata4

--- src/main/scala/kata/kata4/DataMunging.scala
+++ src/main/scala/kata/kata4/DataMunging.scala
@@ -2,18 +2,18 @@ package kata.kata4
 
 import scala.io.Source
 import java.io.InputStream
+import Numeric._
 
 object DataMunging {
 
   type Label = String
-  type Result = Float
 
-  def columns(line: String): Array[String] =
+  def toColumns(line: String): Array[String] =
     line.trim.split(" ").filter(_ != "")
 
   def getRows(fileName: String): Iterator[Array[String]] = {
     val stream: InputStream = getClass.getResourceAsStream(fileName)
-    Source.fromInputStream(stream).getLines().map(columns)
+    Source.fromInputStream(stream).getLines().map(toColumns)
   }
 
   def columnSelector(columnIndexes: Int*): Array[String] => Array[String] =
@@ -23,36 +23,31 @@ object DataMunging {
       ) yield columns(columnIndex)
     }
 
-  def resultCreator(columns: Array[String]): (Label, Result) =
-    (columns(0), columns(1).toFloat - columns(2).toFloat)
-
-  def temperatureFilter(columns: Array[String]): Boolean = true
-
-  def temperatureMapper(columns: Array[String]): (Label, Result) =
+  def temperatureMapper(columns: Array[String]): (Label, Float) =
     (columnSelector(0, 1, 2)
       andThen (a => Array(a(0), a(1).replace("*", ""), a(2).replace("*", "")))
-      andThen resultCreator)(columns)
+      andThen (a => (a(0), a(1).toFloat - a(2).toFloat)))(columns)
 
   def footballFilter(columns: Array[String]): Boolean = columns.length > 8
 
-  def footballMapper(columns: Array[String]): (Label, Result) =
-    (columnSelector(1, 6, 8) andThen resultCreator)(columns)
-
-  def reducer(a: (Label, Result), b: (Label, Result)): (Label, Result) =
-    if (Math.abs(a._2) < Math.abs(b._2)) a else b
-
+  def footballMapper(columns: Array[String]): (Label, Int) =
+    (columnSelector(1, 6, 8) 
+        andThen (a => (a(0), a(1).toInt - a(2).toInt)))(columns)  
+    
+  def reducer[A](a: (Label, A), b: (Label, A))(implicit num: Numeric[A]): (Label, A) =
+      if (num.lt(num.abs(a._2), num.abs(b._2))) a else b
+       
   def main(args: Array[String]): Unit = {
-
+        
     val minTemperatureDiff = getRows("/weather.dat").drop(2)
-      .filter(temperatureFilter)
       .map(temperatureMapper)
-      .reduceLeft(reducer)
+      .reduceLeft(reducer[Float])
     println(minTemperatureDiff)
 
     val minGoalDiff = getRows("/football.dat").drop(1)
       .filter(footballFilter)
       .map(footballMapper)
-      .reduceLeft(reducer)
+      .reduceLeft(reducer[Int])
     println(minGoalDiff)
 
   }
--- src/test/scala/kata/kata4/DataMungingSpec.scala
+++ src/test/scala/kata/kata4/DataMungingSpec.scala
@@ -8,13 +8,13 @@ class DataMungingSpec extends FlatSpec with Matchers {
 
   
   "columns" should "return columns of a line" in { 
-    DataMunging.columns("a") should be (Array("a"))
-    DataMunging.columns(" a ") should be (Array("a"))
-    DataMunging.columns("  a  ") should be (Array("a"))
-    DataMunging.columns("ab") should be (Array("ab"))
-    DataMunging.columns("a b") should be (Array("a","b"))
-    DataMunging.columns("a  b") should be (Array("a","b"))
-    DataMunging.columns("   9  86    32*   59       6  61.5       0.00         240  7.6 220  12  6.0  78 46 1018.6") should be (Array("9", "86", "32*", "59", "6", "61.5", "0.00", "240", "7.6", "220", "12", "6.0", "78", "46", "1018.6"))
+    DataMunging.toColumns("a") should be (Array("a"))
+    DataMunging.toColumns(" a ") should be (Array("a"))
+    DataMunging.toColumns("  a  ") should be (Array("a"))
+    DataMunging.toColumns("ab") should be (Array("ab"))
+    DataMunging.toColumns("a b") should be (Array("a","b"))
+    DataMunging.toColumns("a  b") should be (Array("a","b"))
+    DataMunging.toColumns("   9  86    32*   59       6  61.5       0.00         240  7.6 220  12  6.0  78 46 1018.6") should be (Array("9", "86", "32*", "59", "6", "61.5", "0.00", "240", "7.6", "220", "12", "6.0", "78", "46", "1018.6"))
   }
   
 
